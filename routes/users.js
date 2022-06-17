6640007aeca5c5eb9284c698e0c14631a9d0b7f0
kaito0702 <kaito.kitagawa.biz@gmail.com>
2022-06-17 20:27:58 +0900

    バックエンドのCRUD開発

---
+++ routes/users.js
@@ -0,0 +1,108 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//ユーザー更新
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("ユーザーが更新されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分用のアカウントの時のみ情報を更新できます");
+  }
+});
+
+//ユーザー削除
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("ユーザーが削除されました");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("あなたは自分用のアカウントの時のみ情報を削除できます");
+  }
+});
+
+//ユーザー情報の取得
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//ユーザーのフォロー
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json("フォローに成功しました");
+      } else {
+        return res.status(403).json("既にフォローしています");
+      }
+    } catch (err) {
+      return res.status(500).json(eer);
+    }
+  } else {
+    return res.status(500).json("自分自身をフォローできません");
+  }
+});
+
+//ユーザーフォロー解除
+
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $pull: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json("フォロー解除しました");
+      } else {
+        return res.status(403).json("フォロー解除できません");
+      }
+    } catch (err) {
+      return res.status(500).json(eer);
+    }
+  } else {
+    return res.status(500).json("自分自身をフォロー解除できません");
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//データベース接続
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DB接続中");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//ミドルウェア
+//データをJSONで取得するための記述
+app.use(express.json())
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => console.log("サーバー起動"));
